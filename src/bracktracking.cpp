ec95ad13004430d3ca4b7531487e519658daa65d
Noé Fernandes <noefernandes@ufrn.edu.br>
2021-03-28 18:44:36 -0300

    adicionando teste com instancia 12

--- src/bracktracking.cpp
+++ src/bracktracking.cpp
@@ -1,55 +1,59 @@
 #include "backtracking.h"
 #include <bits/stdc++.h>
 using namespace std;
-#define V 4
+#define V 12
 
 static vector<int> solution;
 static vector<int> best_solution;
 
-Matrix flow;
-
-/*int flow[][V] = {
-        { 0, 2, 3, 6 },
-        { 2, 0, 5, 8 },
-        { 3, 5, 0, 3 },
-        { 6, 8, 3, 0 }
-};*/
 
 
-void tsp(Matrix graph, Matrix flow, vector<bool>& v, int currPos,
+int flow[][V] = {
+        {0, 10,  0,  5,  1,  0,  1,  2,  2, 2, 2, 0},
+		{10,  0,  1,  3,  2,  2,  2,  3, 2, 0, 2, 0},
+ 		{0,  1,  0, 10,  2,  0,  2,  5,  4, 5, 2, 2},
+ 		{5,  3, 10,  0,  1,  1,  5,  0,  0, 2, 1, 0},
+ 		{1,  2,  2,  1,  0,  3,  5,  5,  5, 1, 0, 3},
+ 		{0,  2,  0,  1,  3,  0,  2,  2,  1, 5, 0, 0},
+ 		{1,  2,  2,  5,  5,  2,  0,  6,  0, 1, 5, 5},
+ 		{2,  3,  5,  0,  5,  2,  6,  0,  5, 2, 10, 0},
+ 		{2,  2,  4,  0,  5,  1,  0,  5,  0, 0, 10, 5},
+ 		{2,  0,  5,  2,  1,  5,  1,  2,  0, 0, 0, 4},
+ 		{2,  2,  2,  1,  0,  0,  5, 10, 10,  0, 0, 5},
+ 		{0,  0,  2,  0,  3,  0,  5,  0,  5,  4,  5,  0}
+};
+
+
+void resolve_qap(int graph[][V], int flow[][V], vector<bool>& v, int currPos,
          int n, int count, int cost, int& ans){
 
     if (count == n && graph[currPos][0]) {
     	if(ans > cost + graph[currPos][0] * flow[currPos][0]){
     		best_solution = solution;
     	}
-    	cout << cost + graph[currPos][0] * flow[currPos][0] << "\n";
+    	//cout << cost + graph[currPos][0] * flow[currPos][0] << "\n";
 
     	vector<int> temp = solution;
-    	for(int i = 0; i < temp.size(); i++){
+    	/*for(int i = 0; i < temp.size(); i++){
     		cout << temp[i] << " ";
-    	}
+    	}*/
   
         ans = min(ans, cost + graph[currPos][0] * flow[currPos][0]);
-        cout << "\nresposta: " << ans << "\n";
+        //cout << "\nresposta: " << ans << "\n";
         return;
     }
   
-    // BACKTRACKING STEP
-    // Loop to traverse the adjacency list
-    // of currPos node and increasing the count
-    // by 1 and cost by graph[currPos][i] value
-  
+    
     for (int i = 0; i < n; i++) {
         if (!v[i] && graph[currPos][i]) {
   
-            // Mark as visited
+            // Marcando como visitado
             v[i] = true;
             solution.push_back(i);
-            tsp(graph, flow, v, i, n, count + 1,
+            resolve_qap(graph, flow, v, i, n, count + 1,
                 cost + graph[currPos][i] * flow[currPos][i], ans);
   
-            // Mark ith node as unvisited
+            // Marcando o i-esimo vertice como não visitado
             v[i] = false;
             solution.pop_back();
         }
@@ -57,40 +61,43 @@ void tsp(Matrix graph, Matrix flow, vector<bool>& v, int currPos,
 };
 
 void Backtracking::solve(Qap qap){
-	// n is the number of nodes i.e. V
-    //int n = 4;
-    int n = qap.n;
+	// numero de vertices
+    int n = 12;
   
-    /*int graph[][V] = {
-        { 0, 10, 15, 20 },
-        { 10, 0, 35, 25 },
-        { 15, 35, 0, 30 },
-        { 20, 25, 30, 0 }
+    int graph[][V] = {
+        {0, 1, 2, 3, 4, 1, 2, 3, 4, 5, 2, 3},
+		{1, 0, 1, 2, 3, 2, 1, 2, 3, 4, 3, 2},
+		{2, 1, 0, 1, 2, 3, 2, 1, 2, 3, 4, 3},
+		{3, 2, 1, 0, 1, 4, 3, 2, 1, 2, 5, 4},
+		{4, 3, 2, 1, 0, 5, 4, 3, 2, 1, 6, 5},
+		{1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 1, 2},
+		{2, 1, 2, 3, 4, 1, 0, 1, 2, 3, 2, 1},
+		{3, 2, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2},
+		{4, 3, 2, 1, 2, 3, 2, 1, 0, 1, 4, 3},
+		{5, 4, 3, 2, 1, 4, 3, 2, 1, 0, 5, 4},
+		{2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 0, 1},
+		{3, 2, 3, 4, 5, 2, 1, 2, 3, 4, 1, 0}
     };
-    */
-    Matrix graph;
-    graph = qap.dist;
-    flow = qap.flow;
-
+    
 	vector<bool> v(n);
     for (int i = 0; i < n; i++)
         v[i] = false;
   
-    // Mark 0th node as visited
+    // Marcando 0-esimo vertice como visitado
     v[0] = true;
     int ans = INT_MAX;
 
   	solution.push_back(0);
 
-
-    // Find the minimum weight Hamiltonian Cycle
-    tsp(graph, flow, v, 0, n, 1, 0, ans);
+    resolve_qap(graph, flow, v, 0, n, 1, 0, ans);
   		
     best_solution.push_back(0);
-  	for(int i = 0; i < best_solution.size(); i++){
+  	
+  	cout << "reposta: ";
+  	for(int i = 0; i < n; i++){
   		cout << best_solution[i] << " ";
   	}
-    // ans is the minimum weight Hamiltonian Cycle
-    cout << "\n" << ans;
+    
+    cout << "\ncusto: " << ans << "\n";
   
 }
